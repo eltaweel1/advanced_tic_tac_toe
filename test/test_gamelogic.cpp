2d3f15db982e1de849a883b6457d90d41a0cd0a1
abdo-moamen <abdomoameneltaweel@gmail.com>
2025-04-30 20:32:42 +0300

    Fix unit test call to makeMove with correct arguments

--- test/test_gamelogic.cpp
+++ test/test_gamelogic.cpp
@@ -3,9 +3,12 @@
 
 int main() {
     GameLogic game;
-    game.makeMove(0, 'X');
-    game.makeMove(1, 'X');
-    game.makeMove(2, 'X');
+
+    game.makeMove(0, 0, 'X');
+    game.makeMove(0, 1, 'X');
+    game.makeMove(0, 2, 'X');
+
     assert(game.checkWin('X') == true);
+
     return 0;
 }
