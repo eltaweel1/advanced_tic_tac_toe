641a4c926b319b8381406597bd12d4cea0c8879c
AbdelrahmanEmad11 <abdelrahman.abdullah04@eng-st.cu.edu.eg>
2025-06-18 21:07:35 +0300

    Update ai.h

--- ai.h
+++ ai.h
@@ -10,7 +10,7 @@ class AI {
 public:
     AI(const QString &difficulty, char aiSymbol, char playerSymbol);
     void makeMove(GameLogic *gameLogic);
-
+    
     // Getters for testing purposes
     QString getDifficulty() const { return difficulty; }
     char getAiSymbol() const { return aiSymbol; }
@@ -25,13 +25,13 @@ private:
     std::pair<int, int> findBestMove(GameLogic *gameLogic, int depthLimit = -1);
     int minimax(GameLogic *gameLogic, int depth, bool isMaximizing, int alpha, int beta, int depthLimit);
     int evaluateBoard(GameLogic *gameLogic);
-
+    
     // Difficulty-specific methods
     void makeRandomMove(GameLogic *gameLogic);
     void makeEasyMove(GameLogic *gameLogic);
     void makeMediumMove(GameLogic *gameLogic);
     void makeHardMove(GameLogic *gameLogic);
-
+    
     // Utility methods
     std::vector<std::pair<int, int>> getEmptyCells(GameLogic *gameLogic);
     bool isValidMove(GameLogic *gameLogic, int row, int col);
@@ -39,4 +39,4 @@ private:
     int evaluateLine(char cell1, char cell2, char cell3);
 };
 
-#endif // AI_H
+#endif // AI_H
