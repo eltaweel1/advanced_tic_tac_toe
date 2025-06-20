ed0635a6a38124dd5c21effd15599b3b47791ef0
A-bdelra7man <abodmahmoud195@gmail.com>
2025-06-20 22:51:52 +0300

    Update gamelogic.cpp

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -14,14 +14,14 @@ bool GameLogic::makeMove(int row, int col, char symbol) {
 }
 
 bool GameLogic::checkWin(char symbol) const {
-    // صفوف وأعمدة
+ 
     for (int i = 0; i < 3; ++i) {
         if ((board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) ||
             (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol))
             return true;
     }
 
-    // قطريين
+
     return (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
            (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol);
 }
